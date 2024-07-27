858c9538f5c2ec4aeb401401598ab2f8ee496a1e
Ali Zahir <alizahiruddin@gmail.com>
2024-07-27 16:27:27 +0300

    data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from nlp_text_summarizer.pipeline._01_data_ingestion import DataIngestionTrainingPipeline
 from nlp_text_summarizer.logging import logger
 
-logger.info("This is an info message")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
