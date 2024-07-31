102f60fc8bbd176b42e459b0ad8cc0266c719ed2
Ali Zahir <alizahiruddin@gmail.com>
2024-07-31 20:28:09 +0300

    add model trainer and model evaluation components

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from nlp_text_summarizer.pipeline._01_data_ingestion import DataIngestionTrainin
 from nlp_text_summarizer.logging import logger
 from nlp_text_summarizer.pipeline._02_data_validation import DataValidationTrainingPipeline
 from nlp_text_summarizer.pipeline._03_data_transformation import DataTransformationTrainingPipeline
+from nlp_text_summarizer.pipeline._04_model_trainer import ModelTrainerTrainingPipeline
+from nlp_text_summarizer.pipeline._05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -37,6 +39,31 @@ except Exception as e:
         raise e
 
 
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   # Uncomment the following line to run the model trainer stage. 
+   # Commented the following line to skip the model trainer stage due to limited resources. 
+   
+   # model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
-
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   # Uncomment the following line to run the model trainer stage. 
+   # Commented the following line to skip the model trainer stage due to limited resources. 
+   
+#    model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
